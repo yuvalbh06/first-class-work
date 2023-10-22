b6096b6d37e9e589983ef40ee2c195f3dca2ca14
yuvalbh06 <124523543+yuvalbh06@users.noreply.github.com>
2023-10-22 09:42:22 +0300

    Update gitfile.py

--- gitfile.py
+++ gitfile.py
@@ -1,8 +1,11 @@
 import math
 
 class Dac:
-    def __init__(self, voltage):
+    def __init__(self, voltage, max_voltage, num_of_bits):
         self.voltage = voltage
+        self.max_voltage = max_voltage
+        self.num_of_bits = num_of_bits
+        self.dec_num_of_bits = pow(2, num_of_bits) - 1
 
     def DecimalToBinary(self, num):
         return int("{0:b}".format(math.ceil(num)))
@@ -10,15 +13,15 @@ class Dac:
     def ToDigital(self):
         if self.voltage == 0:
             return 0
-        return self.DecimalToBinary(1023 / (5 / self.voltage))
+        return self.DecimalToBinary(self.dec_num_of_bits / (self.max_voltage / self.voltage))
 
-    def binaryToDecimal(self, n):
-        return int(str(n), 2)
+    def binaryToDecimal(self, num):
+        return int(str(num), 2)
  
     def SetDigitalValue(self, value):
-        return round(5 / (1023 / self.binaryToDecimal(value)), 2)
+        return round(self.max_voltage / (self.dec_num_of_bits / self.binaryToDecimal(value)), 2)
 
-d = Dac(2.5)
+d = Dac(2.5, 5, 10)
 print()
 bin = d.ToDigital()
 print(bin)
